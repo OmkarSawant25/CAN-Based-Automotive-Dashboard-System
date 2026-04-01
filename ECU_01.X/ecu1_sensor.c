fd04b4a98483965a9adf6c0bc7e65dd090255576
OmkarSawant25 <omkarbro2525@gmail.com>
2026-04-01 13:43:27 +0530

    Initial commit - CAN automotive dashboard project

---
+++ ECU_01.X/ecu1_sensor.c
@@ -0,0 +1,60 @@
+#include "ecu1_sensor.h"
+#include "adc.h"
+#include "can.h"
+#include "msg_id.h"
+#include "uart.h"
+#include "matrix_keypad.h"
+#include "clcd.h"
+
+uint16_t adc_val;
+unsigned char speed;
+
+char speed_arr[3];
+char gear_arr[3];
+char key;
+char value;
+
+static char index = 0;
+
+char gear[9][3] = {"ON", "GN", "G1", "G2", "G3", "G4", "G5", "GR", "C "};
+
+uint16_t get_speed() {
+    // Implement the speed function
+    adc_val = read_adc(CHANNEL4);
+    speed = (adc_val * 99) / 1023;
+
+    speed_arr[0] = (speed / 10) + '0';
+    speed_arr[1] = (speed % 10) + '0';
+    speed_arr[2] = '\0';
+
+    clcd_putch((speed / 10) + '0', LINE2(0));
+    clcd_putch((speed % 10) + '0', LINE2(1));
+
+    return speed;
+}
+
+unsigned char get_gear_pos() {
+    // Implement the gear function
+
+    key = scan_switch(); // Scan keypad
+    value = triggering_type(EDGE, key); // Apply edge detection
+
+    if (value == KEY_1) {
+        if (index < 7) {
+            index++;
+        }
+    } else if (value == KEY_4) {
+        if (index > 0) {
+            index--;
+        }
+    } else if (value == KEY_7) {
+        index = 8;
+    }
+
+    gear_arr[0] = gear[index][0];
+    gear_arr[1] = gear[index][1];
+    gear_arr[2] = '\0';
+
+    clcd_print(gear[index], LINE2(8));
+    return 0;
+}
